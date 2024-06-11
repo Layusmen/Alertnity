a03ce4f87a9bb8cd30c40a2d0ec843f591c2f4a9
Olayode Yusuff <getlayus@gmail.com>
2024-06-11 18:18:17 +0100

    checking commit is updated

---
+++ Alertnity/UpvoteDownvote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertnity
+{
+    public class UpvoteDownvote
+    {
+        //tracking the counts of upvotes and downvotes
+        public int Upvotes { get; private set; }
+        public int Downvotes { get; private set; }
+    }
+}
