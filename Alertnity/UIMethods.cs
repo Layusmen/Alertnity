e742929962805b45f854e03ac654c5627853975f
Olayode Yusuff <getlayus@gmail.com>
2024-11-23 22:58:00 +0000

    finetuning the code

--- Alertnity/UIMethods.cs
+++ Alertnity/UIMethods.cs
@@ -10,23 +10,38 @@ namespace Alertnity
 {
     public class UIMethods
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="converter"></param>
         public static void DisplayPostCodeConverterResponse(PostcodeConverter converter)
         {
             Console.WriteLine("Longitude: " + converter.Longitude);
             Console.WriteLine("Latitude: " + converter.Latitude);
             Console.WriteLine("............................");
         }
-
+        
+        /// <summary>
+        /// 
+        /// </summary>
         public static void DisplayNullResponse()
         {
             Console.WriteLine("Response is null.");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="crimeUrl"></param>
         public static void UrlPrint(string crimeUrl)
         {
              Console.WriteLine($"Checking data for: {crimeUrl}");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="month"></param>
         public static void MonthPrint(string month)
         {
             Console.WriteLine($"No data returned for {month}, continuing to next month.");
