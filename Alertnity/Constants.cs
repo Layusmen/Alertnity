fad51b3d34975fa7e7da12cdf9ed7f816eb7da1b
Olayode Yusuff <getlayus@gmail.com>
2025-02-12 18:36:52 +0000

    Solving Issues with postcode api

--- Alertnity/Constants.cs
+++ Alertnity/Constants.cs
@@ -8,7 +8,7 @@ namespace Alertnity
 {
     public class Constants
     {
-        public const string  POSTCODES_IO_URL = "https://api.postcodes.io/postcodes/";
+        public const string  POSTCODES_IO_URL = "https://api.postcodes.io/postcodes";
         public const double RADIUS_METERS = 300.0;
         public const double INVALID_VALUE = 0.0;
         public const int FALSE = 0;
